587ab386854d948c903a769d49fe6397fb711c41
dudrmsek <dudrmsek@gmail.com>
2023-06-10 14:28:28 +0900

    index page 추가

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-from api import (doraemong,)
+from api import (
+    doraemong,
+)
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"code": 200, "message": "nead main page"}
-
 app.include_router(doraemong)
 
-app.mount("/doraemong", StaticFiles(directory="static/doraemong",html = True), name="doraemong")
+app.mount("/", StaticFiles(directory="static", html=True), name="index")
+app.mount("/doraemong", StaticFiles(directory="static/doraemong",
+          html=True), name="doraemong")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.0.14", port=2030)
