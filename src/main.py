157826bc0cd00604813e3963c0743d7d3b238c1d
dudrmsek <dudrmsek@gmail.com>
2023-07-13 20:32:29 +0900

    :bugs:router/mount 수정

--- src/main.py
+++ src/main.py
@@ -7,16 +7,14 @@ import uvicorn
 from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
-app.include_router(doraemong)
-app.include_router(idol_position_router)
 
+for router in [doraemong, idol_position_router]:
+    app.include_router(router)
+    static_directory = f"static/{router.prefix}"
+    app.mount(f"/{router.prefix}", StaticFiles(directory=static_directory,
+              html=True), name=router.prefix)
 
 app.mount("/", StaticFiles(directory="static", html=True), name="index")
-app.mount("/doraemong", StaticFiles(directory="static/doraemong",
-          html=True), name="doraemong")
-app.mount("/Idol_position_test", StaticFiles(directory="static/Idol_position_test",
-          html=True), name="Idol_position_test")
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.0.14", port=2030)
