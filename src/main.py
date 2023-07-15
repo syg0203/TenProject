903aaee55015b545284a9b382dbdbaa515bd352b
dudrmsek <dudrmsek@gmail.com>
2023-07-15 13:16:45 +0900

    병합

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI
-from api import (
-    doraemong,
-    idol_position_router,
-)
+from api import doraemong, IdolPosition
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
+routers = ["doraemong", "IdolPosition"]
 
-for router in [doraemong, idol_position_router]:
-    app.include_router(router)
-    static_directory = f"static/{router.prefix}"
-    app.mount(f"/{router.prefix}", StaticFiles(directory=static_directory,
-              html=True), name=router.prefix)
+for router in routers:
+    print(router)
+    exec(f"app.include_router({router})")
+    app.mount(f"/{router}", StaticFiles(directory=f"static/{router}",
+              html=True), name=router)
 
 app.mount("/", StaticFiles(directory="static", html=True), name="index")
 
