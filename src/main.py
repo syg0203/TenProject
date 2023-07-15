2296a6e44c79bb93bd527ddba65cc3d0d358db65
dudrmsek <dudrmsek@gmail.com>
2023-07-15 13:58:41 +0900

    병합

--- src/main.py
+++ src/main.py
@@ -1,22 +1,11 @@
 from fastapi import FastAPI
-<<<<<<< HEAD
 from api import doraemong, IdolPosition
-=======
-from api import (
-    doraemong,
-    IdolPosition,
-)
->>>>>>> origin
+
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
-<<<<<<< HEAD
 routers = ["doraemong", "IdolPosition"]
-=======
-app.include_router(doraemong)
-app.include_router(IdolPosition)
->>>>>>> origin
 
 for router in routers:
     print(router)
@@ -25,14 +14,6 @@ for router in routers:
               html=True), name=router)
 
 app.mount("/", StaticFiles(directory="static", html=True), name="index")
-<<<<<<< HEAD
-=======
-app.mount("/doraemong", StaticFiles(directory="static/doraemong",
-          html=True), name="doraemong")
-app.mount("/IdolPosition", StaticFiles(directory="static/IdolPosition",
-          html=True), name="IdolPosition")
-
->>>>>>> origin
 
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.0.14", port=2030)
