2bb184aa59fb61cf7e095f686d8230b1c4261cd4
dudrmsek <dudrmsek@gmail.com>
2023-07-18 23:29:23 +0900

    :bug:존재자체가 버그

--- src/main.py
+++ src/main.py
@@ -1,18 +1,37 @@
 from fastapi import FastAPI, Request
-from api import doraemong, IdolPosition
 from fastapi.responses import JSONResponse
-
-import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+import uvicorn
+# import sys
+# import os
+
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(current_dir)
+from api import doraemong, IdolPosition
+
 app = FastAPI()
 
 routers = ["doraemong", "IdolPosition"]
 
+origins = [
+    "http://localhost",
+    # "http://localhost:2030",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.middleware("http")
 async def access_control_middleware(request: Request, call_next):
+    print(f"Received request: {request.method} {request.url}")
     # 원하는 엔드포인트 경로를 확인하여 접근 제한 로직을 적용할 수 있습니다.
-    if request.url.path in ["/docs", '/redoc']:
+    if request.url.path in ['/redoc']:
         # 접근 제한에 해당하는 경우 응답을 반환하고 요청 처리를 종료합니다.
         return JSONResponse(status_code=403, content={"detail": "Access denied"})
 
@@ -28,4 +47,4 @@ for router in routers:
 app.mount("/", StaticFiles(directory="static", html=True), name="index")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="192.168.0.14", port=2030)
+    uvicorn.run(app, host="0.0.0.0", port=2030)
