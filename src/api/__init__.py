6d027daa38c90d7b6d9b14524ab0ff96f2e7b7fc
dudrmsek <dudrmsek@gmail.com>
2023-09-13 16:05:06 +0900

    :zap:add solo version

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,4 +1,5 @@
 from api.doraemong import route as doraemong
 from api.IdolPosition import router as IdolPosition
+from api.doraemong_solo import route as doraemong_solo
 
-__all__ = ["doraemong", "IdolPosition"]
+__all__ = ["doraemong", "IdolPosition", "doraemong_solo"]
